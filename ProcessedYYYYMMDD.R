f38bdd883554f4acb559b9f5b540ceaa695b18ba
nlevesq <55558723+nlevesq@users.noreply.github.com>
2020-02-23 15:51:45 -0300

    Updated Version of Processing Script
    removes duplicate columns
    changed some default object names
    added further annotations

--- ProcessedYYYYMMDD.R
+++ ProcessedYYYYMMDD.R
@@ -1,17 +1,23 @@
 #Reads in the file
-run6<-read.csv("ExportYYYYMMDD.csv", header = T)
+run<-read.csv("ExportYYYYMMDD.csv", header = T)
 
 #Removes all the blanks
-run6a<-subset(run6, Identifier.1!='blank')
+runa<-subset(run, Identifier.1!='blank')
 
 #Should gather the Nitrogen data and ignore the reference peaks
-run6N<-subset(run6a, Peak.Nr==4)
+runN<-subset(runa, Peak.Nr==4)
 
 #Should gather the Carbon data and ignore the reference peaks
-run6C<-subset(run6a, Peak.Nr==5)
+runC<-subset(runa, Peak.Nr==5)
 
 #Shoves the Carbon and Nitrogen data together
-run6b<-merge(run6C, run6N, by='Row', no.dups = F)
+runb<-merge(runC, runN, by='Row', no.dups = T)
 
-#Exports a file into your working directory that must be varified and further cleaned up 
-write.csv(run6b, "ProcessedYYYYMMDD.csv")
+#Keeps only the necessary rows 
+columns<-c('Row', 'Identifier.1.x',	"Identifier.2.x",	
+            "Amount.x",	"d.13C.12C.x",	'd.15N.14N.y')
+processed<-subset(runb, select = columns)
+
+#Exports a file into your working directory that must be verified and further cleaned up 
+###Change this to a new file name###
+write.csv(processed, "ProcessedYYYYMMDD.csv")
